44e0f5bb310f30fb98dd5e350715bf088330e2e4
Sid <skaytacium@gmail.com>
2022-12-13 19:56:16 +0530

    New colors

--- run.sh
+++ run.sh
@@ -3,30 +3,29 @@
 #1 - Verb(s)
 #2 - Name
 
-[ "$#" -lt 2 ] && (echo "\033[4;1;31m*** not enough parameters\033[0m"; exit 1)
-
 ROOT_DIR="$(dirname -- "$0")"
 
 alias log="$ROOT_DIR/out.sh 0"
 alias warn="$ROOT_DIR/out.sh 1"
 alias error="$ROOT_DIR/out.sh 2"
-alias head="$ROOT_DIR/out.sh 3"
+
+[ "$#" -lt 2 ] && (error "not enough parameters")
 
 for NAME in $2; do
 	for VERB in $1; do
 		echo
-		head "$VERB $NAME"
+		log "$VERB $NAME"
 
 		if [ -x "$ROOT_DIR/verbs/$VERB/$NAME.sh" ]; then
-			"$ROOT_DIR/verbs/$VERB/$NAME.sh" &
+			"$ROOT_DIR/verbs/$VERB/$NAME.sh"
 		elif [ -x "$ROOT_DIR/verbs/$VERB/default.sh" ]; then
-			warn "$VERB $NAME: defaulting"
-			"$ROOT_DIR/verbs/$VERB/default.sh" &
+			warn "defaulting"
+			"$ROOT_DIR/verbs/$VERB/default.sh"
 		else
-			error "$VERB $NAME: no script found"
+			error "no script found"
 		fi
 
-		head "$VERB $NAME: done"
+		log "/$VERB $NAME"
 	done
 
 	wait
