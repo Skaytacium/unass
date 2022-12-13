ec3b5a4ced7f805f1a91d1b41e33b09e17688e25
Sid <skaytacium@gmail.com>
2022-12-13 20:36:57 +0530

    finally

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-if ! [ "$(basename $(pwd))" = "unass" ]; then
-	mkdir unass
-	cd unass
-fi
+echo "installing"
+
+[ -d "unass" ] || mkdir unass
+
+cd unass
 
 VERBS="apply build clone compile download extract install service sync"
 
-if ! [ -x "./run" ]; then
+if ! [ -x "./run.sh" ]; then
 	mkdir verbs
 else
 	VERBS="$(find verbs -mindepth 1 -maxdepth 1 -exec 'basename' '{}' ';')"
@@ -17,11 +18,21 @@ cd verbs
 
 for VERB in $VERBS; do
 	[ "$VERB" = "build" ] && ln -sf compile build && continue
-	mkdir "$VERB"
-	curl -fL "https://raw.githubusercontent.com/Skaytacium/unass/master/verbs/$VERB/default.sh" > "$VERB/default.sh"
+	echo "downloading $VERB verb"
+	[ -d "$VERB" ] || mkdir "$VERB"
+	curl -fL "https://raw.githubusercontent.com/Skaytacium/unass/master/verbs/$VERB/default.sh" > "$VERB/default.sh" 2>/dev/null &
 done
+
+wait
+
+echo "permitting verbs"
+chmod +x */default.sh
 
 cd ..
 
-curl -fL "https://raw.githubusercontent.com/Skaytacium/unass/master/run.sh" > "run.sh"
-curl -fL "https://raw.githubusercontent.com/Skaytacium/unass/master/out.sh" > "out.sh"
+echo "downloading base scripts"
+curl -fL "https://raw.githubusercontent.com/Skaytacium/unass/master/run.sh" > "run.sh" 2>/dev/null
+curl -fL "https://raw.githubusercontent.com/Skaytacium/unass/master/out.sh" > "out.sh" 2>/dev/null
+
+echo "permitting base scripts"
+chmod +x *.sh
