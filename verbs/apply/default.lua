4ea1206d517ec76256ea294579c8f49508b2ae1f
Sid <skaytacium@gmail.com>
2023-01-27 18:54:20 +0530

    feat: Finish default apply verb

--- verbs/apply/default.lua
+++ verbs/apply/default.lua
@@ -1,14 +1,36 @@
+---@param file string
+---@param branch string
+local function cp(file, branch)
+	return FIND_IN_TABLE(TRY_UNTIL({
+		("cd %s; git cat-file -p %s:%s > /%s")
+		:format(OPTIONS.store, branch, file, file),
+		("couldn't apply %s as user, elevating"):format(file),
+		("applied %s as user"):format(file)
+	}, {
+		("cd %s; git cat-file -p %s:%s | %s tee /%s")
+		:format(OPTIONS.store, branch, file, OPTIONS.elevate, file),
+		("couldn't apply %s as root"):format(file),
+		("applied %s as root"):format(file)
+	}), 0)
+end
+
 ---@param inode string
 ---@param branch string
 local function apply(inode, branch)
-	local filetype = io.popen(("cd %s; git cat-file -t %s:%s"):format(OPTIONS.store, branch, inode)):read()
+	local filetype = io.popen(("cd %s; git cat-file -t %s:%s 2>/dev/null"):format(OPTIONS.store, branch, inode)):read()
 
 	if filetype == "tree" then
-		print("1")
-		print(io.popen(("cd %s; git ls-tree %s:%s"):format(OPTIONS.store, branch, inode)))
-	elseif filetype == "blob" then
-		print("2")
-		print(io.popen(("cd %s; git cat-file -p %s:%s"):format(OPTIONS.store, branch, inode)))
+		local files = io.popen(("cd %s; git ls-tree -r --name-only %s:%s"):format(OPTIONS.store, branch, inode))
+		---@diagnostic disable-next-line: need-check-nil
+		local file = files:read()
+
+		repeat
+			cp(inode .. "/" .. file, branch)
+
+			---@diagnostic disable-next-line: need-check-nil
+			file = files:read()
+		until not file
+	elseif filetype == "blob" then cp(inode, branch)
 	else return end
 end
 
@@ -17,11 +39,6 @@ end
 return function(inode, branch)
 	branch = branch and branch or OPTIONS.branch
 
-	local default_branch = GET_DEFAULT_BRANCH()
-
-	print(inode)
-	print(branch)
-
-	apply(inode, default_branch)
+	apply(inode, GET_DEFAULT_BRANCH())
 	apply(inode, branch)
 end
